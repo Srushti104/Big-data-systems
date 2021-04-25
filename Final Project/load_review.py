7ba14c9457df5a0cee44e6a7d2ec9c7ce2bfbc4f
srushti104 <dhamangaonkar.s@northeastern.edu>
2021-04-25 13:57:23 -0400

    update: scraper script

---
+++ Final Project/load_review.py
@@ -0,0 +1,41 @@
+import json
+import boto3
+import pandas as pd
+# from smart_open import smart_open
+
+DYNAMODB = boto3.resource('dynamodb')
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('ids706')
+data_key = 'bitcoin_reddit_all.csv.zip'
+data_location = 's3://{}/{}'.format(bucket.name, data_key)
+
+
+def convert(filename):
+    final = pd.read_csv(filename, low_memory=False)
+
+    listOfDict = []
+
+    for index, row in final.iterrows():
+        newDict = {'time': '', 'body': ''}
+        newDict['Date'] = row['date']
+        newDict['text'] = row['body']
+        listOfDict.append(newDict)
+        pass
+
+    return listOfDict
+
+
+def load(dictName, dynamodb):
+    table = dynamodb.Table('CommentTable')
+
+    for i in range(len(dictName)):
+        response = table.put_item(
+            Item={
+                'Date': dictName[i]['Date'],
+                'body': dictName[i]['text']
+            }
+        )
+
+
+if __name__ == '__main__':
+    load(convert("bitcoin_review_20210424.csv"), DYNAMODB)
